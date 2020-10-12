c89618dad6f0a673076a662d2e46ff54167ae069
Emma <Emma@Mathiass-MacBook-Pro-3.local>
2020-10-12 13:58:27 +0200

    index.html og index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+    const divs = section.querySelectorAll("div");
+    gsap.from(divs, {
+        opacity: 0,
+        y: 100,
+        duration: 4,
+        stagger: 3,
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            end: "center center",
+            markers: "true",
+            scrub: "true",
+        }
+    })
+})
